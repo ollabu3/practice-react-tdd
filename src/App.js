a1f6e13b6806faabd08e71027b6db4d428dec618
ollabu3 <ollabu3@gmail.com>
2022-01-11 15:38:07 +0900

    terms-and-conditions-popover test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-export function replaceCametWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, " $1");
-}
+import OrderSummary from "./pages/summary/OrderSummary";
+
 const App = () => {
-  const [buttonColor, setButtonColor] = useState("blue");
-  const [disabled, setDisabled] = useState(false);
-  const changeColor = () =>
-    disabled ? setButtonColor("gray") : setButtonColor("blue");
-
-  useEffect(() => {
-    changeColor();
-  }, [disabled]);
-
   return (
     <div className="App">
-      <button
-        style={{
-          backgroundColor: buttonColor,
-          border: "none",
-          cursor: "pointer",
-        }}
-        disabled={disabled}
-      >
-        Change to {buttonColor}
-      </button>
-      <input
-        id="disable-button-checkbox"
-        type="checkbox"
-        defaultChecked={disabled}
-        aria-checked={disabled}
-        onClick={() => setDisabled(!disabled)}
-      />
-      <label htmlFor="disable-button-checkbox">checkbox disabled</label>
+      <OrderSummary />
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
-import { replaceCametWithSpaces } from "./App";
-// test("button turns blue and red when clicked", () => {
+
+// test("button disabled when checkbox checked", () => {
 //   render(<App />);
-//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-//   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
-//   fireEvent.click(colorButton);
-//   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
-//   expect(colorButton.textContent).toBe("Change to red");
 // });
-
-test("button disabled when checkbox checked", () => {
-  render(<App />);
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  const checkbox = screen.getByRole("checkbox", { name: "checkbox disabled" });
-
-  expect(colorButton).toBeEnabled();
-  expect(checkbox).not.toBeChecked();
-  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
-
-  fireEvent.click(checkbox);
-  expect(colorButton).toBeDisabled();
-  expect(checkbox).toBeChecked();
-  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
-
-  fireEvent.click(checkbox);
-  expect(colorButton).toBeEnabled();
-  expect(checkbox).not.toBeChecked();
-  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
-});
-
-describe("spaces before camel-case capital letters", () => {
-  test("works for no inner capital letters", () => {
-    expect(replaceCametWithSpaces("Red")).toBe("Red");
-  });
-  test("works for no inner capital letter", () => {
-    expect(replaceCametWithSpaces("MidnightBlue")).toBe("Midnight Blue");
-  });
-  test("works for multiple inner capital letters", () => {
-    expect(replaceCametWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
-  });
-});
